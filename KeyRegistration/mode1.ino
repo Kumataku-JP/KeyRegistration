72e4e2b1b8d9c746e3f3ce9e419782fc297ac3f2
Kumataku <61592773+Kumataku-JP@users.noreply.github.com>
2024-09-23 22:18:48 +0900

    Add files via upload

--- KeyRegistration/mode1.ino
+++ KeyRegistration/mode1.ino
@@ -2,27 +2,27 @@ void mode1() {
   keys = read_LCD_buttons(analogRead(0));
 
   if (keys == btnUP) {
-    leftStickTilt(0, 100);
+    stickTilt(Stick::LEFT, 0, 100);
     lcd.setCursor(2, 0);
     lcd.cursor();
     delay(250);
   } else if (keys == btnDOWN) {
-    leftStickTilt(180, 100);
+    stickTilt(Stick::LEFT, 180, 100);
     lcd.setCursor(2, 0);
     lcd.cursor();
     delay(250);
   } else if (keys == btnLEFT) {
-    leftStickTilt(270, 100);
+    stickTilt(Stick::LEFT, 270, 100);
     lcd.setCursor(2, 0);
     lcd.cursor();
     delay(250);
   } else if (keys == btnRIGHT) {
-    leftStickTilt(90, 100);
+    stickTilt(Stick::LEFT, 90, 100);
     lcd.setCursor(2, 0);
     lcd.cursor();
     delay(250);
   } else {
-    leftStickNeutral();
+    stickNeutral(Stick::LEFT);
     lcd.noCursor();
   }
 }
--- KeyRegistration/mode2.ino
+++ KeyRegistration/mode2.ino
@@ -2,27 +2,27 @@ void mode2() {
   keys = read_LCD_buttons(analogRead(0));
 
   if (keys == btnUP) {
-    rightStickTilt(0, 100);
+    stickTilt(Stick::RIGHT, 0, 100);
     lcd.setCursor(2, 0);
     lcd.cursor();
     delay(250);
   } else if (keys == btnDOWN) {
-    rightStickTilt(180, 100);
+    stickTilt(Stick::RIGHT, 180, 100);
     lcd.setCursor(2, 0);
     lcd.cursor();
     delay(250);
   } else if (keys == btnLEFT) {
-    rightStickTilt(270, 100);
+    stickTilt(Stick::RIGHT, 270, 100);
     lcd.setCursor(2, 0);
     lcd.cursor();
     delay(250);
   } else if (keys == btnRIGHT) {
-    rightStickTilt(90, 100);
+    stickTilt(Stick::RIGHT, 90, 100);
     lcd.setCursor(2, 0);
     lcd.cursor();
     delay(250);
   } else {
-    rightStickNeutral();
+    stickNeutral(Stick::RIGHT);
     lcd.noCursor();
   }
 }
